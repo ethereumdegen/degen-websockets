2b25b8366c30ff3988de747341242bcfb2db0141
andy <admazzola@gmail.com>
2023-07-23 21:34:52 -0400

    add lib

--- src/core_data_message.rs
+++ src/core_data_message.rs
@@ -3,8 +3,8 @@
 use serde::{Serialize,Deserialize};
 use tokio_tungstenite::tungstenite::Message;
 
-use crate::net::message_types::{  ClientMessage, ServerMessage, InternalServerMessage };
-use crate::websockets::websocket_messages::{MessageReliability};
+ 
+use crate::websocket_messages::{MessageReliability};
 
 use super::websocket_messages::MessageReliabilityType;
 use thiserror::Error;
@@ -20,12 +20,6 @@ use thiserror::Error;
 #[serde(tag = "msg_type", content = "data")]
 pub enum CoreDataMessageContents {
 
-    //make sure these uuids cant be cheated / spoofed !?   may need to change architecture ? s
-  //UnconnectedClientMsg{ socket_connection_uuid:String, client_message: ClientMessage }, //like for client register, the relay is going to have to await a response and feed it back to client 
-  //ConnectedClientMsg{ client_uuid: String,  client_message: ClientMessage} ,  //for when client is already connected 
-  ClientMsg(ClientMessage),
-  ServerMsg(ServerMessage),
-  InternalMsg(InternalServerMessage),
   
 
 }
@@ -59,11 +53,8 @@ impl MessageReliability for CoreDataMessageContents {
     fn get_reliability_type(&self, message_uuid:String ) -> MessageReliabilityType {  
          
     
-        match self {
-            
-            
-            CoreDataMessageContents::InternalMsg( .. ) => MessageReliabilityType::Reliable( message_uuid ),
-            
+        match self { 
+             
             
             _ => MessageReliabilityType::Unreliable
         }
