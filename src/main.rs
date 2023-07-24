8701dcd6102510fef46fb914634fb51b17fbd313
andy <admazzola@gmail.com>
2023-07-23 21:28:05 -0400

    adding version

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ pub mod reliable_message_subsystem;
 pub mod websocket_messages;
 pub mod util;
 
+use std::sync::Arc;
+
 use crate::websocket_server::WebsocketServer;
 use util::websocket_channel::TokioMpscChannel;
 use websocket_messages::{MessageReliability, MessageReliabilityType};
@@ -16,6 +18,11 @@ use crate::websocket_client::WebsocketClient;
 use tokio::time::Duration;
 
 use serde::{Serialize,Deserialize};
+
+
+// ----
+
+// You can define your own inner messages to send through the socket server.  They just need to implement Serialize/Deserialize and MessageReliability as follows: 
 
 #[derive(Serialize,Deserialize)]
 struct MyCustomMessage {
@@ -30,11 +37,14 @@ impl MessageReliability for MyCustomMessage {
          }
 }
 
+// -----
+
+
 
 #[tokio::main]
 async fn main() {
    
-    let rt = tokio::runtime::Runtime::new().unwrap();
+    let rt = Arc::new( tokio::runtime::Runtime::new().unwrap() );
 
     // no ws:// here 
     let server_url:String = "localhost:8100".to_string();
@@ -61,8 +71,11 @@ async fn main() {
         loop {
             let recv_msg_result  = websocket_server_rx.recv().await;
        
-       
-            println!("received a message!");
+            if let Some(recv_msg) = recv_msg_result {
+                   println!("server received a message! {}" ,recv_msg );
+                
+            }
+         
         }
       
     }); 
@@ -70,16 +83,19 @@ async fn main() {
 
     //need ws here 
     let remote_server_url:String = "localhost:8100".to_string();
-
     
-    rt.spawn(async move {
+    let base_runtime = Arc::clone(&rt);
+    let inner_runtime = Arc::clone(&base_runtime);
+    let recv_runtime = Arc::clone(&base_runtime);
+    
+    base_runtime.spawn(async move {
         tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
                  
           //boot a client 
                 
         let mut socket_conn_recv_channel: TokioMpscChannel<InboundMessage>  = TokioMpscChannel::new(500)  ; 
                                     
-        let socket_conn_recv_rx = socket_conn_recv_channel.rx.take().unwrap();
+        let mut socket_conn_recv_rx = socket_conn_recv_channel.rx.take().unwrap();
     
          let mut  socket_client = WebsocketClient::new();  
          let socket_conn_recv_tx_for_thread = socket_conn_recv_channel.tx.clone();
@@ -99,9 +115,23 @@ async fn main() {
           
                
            
-           //never goes past this ?
            
 
+
+          
+            //start the client listening thread -- this will await forever  in this spawned rt 
+               inner_runtime.spawn(async move {
+                    let client_listen_thread= socket_connection. listen( 
+                                
+                            socket_conn_recv_tx_for_thread.clone()
+                        ).await;  
+                });
+                
+                
+                
+                //send messages from client to server !! 
+                //when server sends msgs, they will fill up 'socket_conn_recv_rx' buffer so we can check that buffer for them :) 
+                
            let destination = websocket_messages::SocketMessageDestination::All;
             
            let socket_message_result = SocketMessage ::create(  destination, MyCustomMessage{color:"blue".to_string()}); 
@@ -111,14 +141,17 @@ async fn main() {
            }else{
                println!("could not create message");
            }
-
-          
-            //start the client listening thread -- this will await forever 
-             let client_listen_thread= socket_connection. listen( 
-                        
-                    socket_conn_recv_tx_for_thread.clone()
-                ).await;  
             
+            
+            recv_runtime.spawn(async move {
+                loop {
+                        if let Some(inbound_msg) = socket_conn_recv_rx.recv().await {
+                            println!( "client got a message from the server {}" , inbound_msg );
+                            
+                        }
+                         
+                }
+            });
             
         }
 
